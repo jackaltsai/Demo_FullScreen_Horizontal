d57bc091876900bec7c587600d827dd82a5d80ba
jackaltsai <hata.s520@gmail.com>
2021-09-20 22:49:57 +0800

    全螢幕照片，滑動時可看到間距，滑動停止時不會看到間距

---
+++ Demo_FullScreen_Horizontal/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Demo_FullScreen_Horizontal
+//
+//  Created by 蔡忠翊 on 2021/9/20.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
--- Demo_FullScreen_Horizontal/ViewController.swift
+++ Demo_FullScreen_Horizontal/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.collectionViewLayout = generateLayout()
+        collectionView.isScrollEnabled = false
     }
 
-
+    
+    // 全螢幕照片，滑動時可看到間距，滑動停止時不會看到間距
+    // orthogonalScrollingBehavior 設為 groupPaging，利用 section 的. interGroupSpacing 設定間距。另外記得要將 collectionView 的 isScrollEnabled 設為 false，不然畫面還是可以垂直捲動
+    // image view 的 content mode 設為 aspect fit
+    
+    func generateLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        section.orthogonalScrollingBehavior = .groupPaging
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(CollectionViewCell.self)", for: indexPath) as! CollectionViewCell
+        cell.imageView.image = UIImage(named: "Image\(indexPath.item + 1)")
+        return cell
+    }
+    
+    
+}
